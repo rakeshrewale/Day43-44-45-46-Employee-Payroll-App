11c6944085dcbd382273cf4e12682bfcdaf7ed16
rakeshrewale <rakeshrewale993@gmail.com>
2022-07-26 12:59:53 +0530

    New attributes and getters and setters

---
+++ js/EmployeePayroll.js
@@ -0,0 +1,80 @@
+class EmployeePayrollData {
+    get id() {
+      return this._id;
+    }
+  
+    set id(id) {
+      this._id = id;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(name) {
+      let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+      if (nameRegex.test(name)) this._name = name;
+      else throw "Name is Incorrect";
+    }
+  
+    get profilePic() {
+      return this._profilePic;
+    }
+    set profilePic(profilePic) {
+      this._profilePic = profilePic;
+    }
+  
+    get gender() {
+      return this._gender;
+    }
+  
+    set gender(gender) {
+      this._gender = gender;
+    }
+  
+    get department() {
+      return this._department;
+    }
+  
+    set department(department) {
+      this._department = department;
+    }
+  
+    get salary() {
+      return this._salary;
+    }
+  
+    set salary(salary) {
+      this._salary = salary;
+    }
+  
+    get note() {
+      return this._note;
+    }
+  
+    set note(note) {
+      this._note = note;
+    }
+  
+    get startDate() {
+      return this._startDate;
+    }
+  
+    set startDate(startDate) {
+      let actualDate = new Date(startDate).getTime();
+      let currentDate = new Date().getTime();
+      if (actualDate > currentDate) {
+        throw "Future Date. Date is Invalid";
+      } else this._startDate =stringifyDate(new Date(startDate).toLocaleDateString());
+    }
+    
+    // method
+    toString() {
+      const options = { year: "numeric", month: "long", day: "numeric" };
+      const empDate = this.startDate === undefined ? "undefined" : this.startDate;
+      return ("id=" +this.id +", name=" +this.name +", gender=" +this.gender +", profilePic=" +this.profilePic +
+        ", department=" +this.department +", salary=" +this.salary +", " +"gender=" +this.gender +", startDate=" +
+        empDate +", notes= " +this.note
+      );
+    }
+  }
